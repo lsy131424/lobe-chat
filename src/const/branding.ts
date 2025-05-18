ab2636abc89604d3d4263321803269edd2bb12a5
lsy131424 <3309778572@qq.com>
2025-05-18 10:52:55 +0800

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = '基于多模态大模型的彝族跳菜文化数字保护与传播';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = '基于多模态大模型的彝族跳菜文化数字保护与传播';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = '基于多模态大模型的彝族跳菜文化数字保护与传播';
 
 export const BRANDING_URL = {
   help: undefined,
